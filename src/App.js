0fb281f16fcf4ac8c137fcb7fee2a7b34b449e48
hamzayaseen15 <hamzayaseen993@gmail.com>
2023-10-17 18:52:35 +0500

    add dark mode to complete project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import Textform from './components/Textform';
+import Textform from './components/Textform';
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [textColor, setTextColor] = useState('dark');
+  const [text, setText] = useState('Enable Dark Mode');
+
+  const toggleMode =()=>{
+    if (mode === 'light'){
+      setMode('dark');
+      setTextColor('light');
+      setText('Disable Dark Mode');
+      document.body.style.backgroundColor = 'black'
+    }
+    else{
+      setMode('light');
+      setTextColor('dark');
+      setText('Enable Dark Mode');
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
     <>
-    <Navbar title = "TextUtils"  about = "AboutUs"/>
-    <div className="container">
-    {/* <Textform heading = "Please Enter the text"/> */}
-    <About/>
+    <Navbar title = "TextUtils"  about = "AboutUs" mode = {mode} toggleMode = {toggleMode} textColor = {textColor} text = {text}/>
+    <div className="container" >
+    <Textform heading = "Please Enter the text" mode={mode}/>
+    {/* <About/> */}
     </div>
     </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -42,24 +42,24 @@ export default function Textform(props) {
 
     return (
         <>
-          <div className='container'>
+          <div className='container' style={{color: props.mode==='light'?'black':'white'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-              <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+              <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'black', color: props.mode==='light'?'black':'white'}} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to lowercase</button>
             <button className="btn btn-secondary mx-2" onClick={searchEmail}>Find Emails</button>
             <button className="btn btn-success mx-2" onClick={handleClearClick}>Clear</button>
           </div>
-          <div className='container my-3'>
+          <div className='container my-3'style={{color: props.mode==='light'?'black':'white'}} >
             <h1>Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} mins to read all characters</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length > 0? text : "Enter some text to preview it"}</p>
           </div>
-          <div className="container my-3">
+          <div className="container my-3" style={{color: props.mode==='light'?'black':'white'}}>
             {noEmailsFound ? (
               <h3>No email addresses found .</h3>
             ) : (
